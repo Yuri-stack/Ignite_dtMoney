1ff1d53e76604c2639490e33842002f0866810e5
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-09-03 21:57:13 -0300

    Feat.: Start API Model with Fake data through Seeds and list transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction{
+    id: number
+    title: string
+    amount: number
+    type: string
+    category: string
+    createdAt: string
+}
+
 export function TransactionsTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
     useEffect(() => {
         api.get('transactions')
-            .then(response => console.log(response.data))
+            .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return(
@@ -19,19 +30,16 @@ export function TransactionsTable(){
                         <th>Data</th>
                     </tr>
                 </thead>
+
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>31/08/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$1.200</td>
-                        <td>Casa</td>
-                        <td>31/08/2021</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                        <tr key={ transaction.id }>
+                            <td>{ transaction.title }</td>
+                            <td className={ transaction.type }>{ transaction.amount }</td>
+                            <td>{ transaction.category }</td>
+                            <td>{ transaction.createdAt }</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
