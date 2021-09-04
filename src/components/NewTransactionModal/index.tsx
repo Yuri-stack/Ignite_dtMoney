56b23bc58a5a0f7f516e21d74530384712b08a30
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-09-04 14:33:19 -0300

    Feat.: Add a hook to organize the codes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,12 @@
-import { FormEvent, useState, useContext } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
-
-import { TransactionsContext } from '../../TransactionsContext';
 
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { useTransactions } from '../../hooks/useTransactions';
 
 interface NewTransactionModalProps{
     isOpen: boolean
@@ -15,7 +14,7 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    const { createTransactions } = useContext(TransactionsContext)
+    const { createTransactions } = useTransactions()
     
     const [type, setType] = useState('deposit')
     const [title, setTitle] = useState('')
