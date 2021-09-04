6cfa7917a0674e9daab85e90e2b69a2853e88a22
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-09-03 21:42:10 -0300

    Feat.: Using the MirageJS API to record transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 
+import { api } from '../../services/api';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps{
@@ -20,12 +21,9 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
     function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault()
-        console.log({
-            title,
-            value,
-            category,
-            type
-        })
+        const data = ({ title, value, category, type })
+
+        api.post('/transactions', data)
     }
     
     return (
